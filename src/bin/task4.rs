5ed58dec0402f99bd7dd555a0ef4eaa9f0b20490
Fox_white <vct.xie@gmail.com>
2023-12-02 17:51:22 +0800

    feat: add two way channel

--- src/bin/task4.rs
+++ src/bin/task4.rs
@@ -5,21 +5,37 @@ use hello::{
 
 fn main() {
     let channel = MessageQueue::new(5).unwrap();
+    let ack_channel = MessageQueue::new(5).unwrap();
+
     let clonned = channel.clone();
+    let clonned_ack = ack_channel.clone();
 
     task_spawn("send", 90, move || {
         for i in 0..10 {
-            let value =
-                Some(format!("Hello World From other task though message queue: i = {}", i));
-            clonned.send(value, WAIT_FOREVER, MessageQueuePriority::Normal).unwrap();
+            let value = Some(format!(
+                "Hello World From other task though message queue: i = {}",
+                i
+            ));
+            clonned
+                .send(value, WAIT_FOREVER, MessageQueuePriority::Normal)
+                .unwrap();
+            let ack = clonned_ack.receive(WAIT_FOREVER).unwrap();
+            if ack {
+                println!("send: ack from rec")
+            }
         }
-        clonned.send(None, WAIT_FOREVER, MessageQueuePriority::Normal).unwrap();
+        clonned
+            .send(None, WAIT_FOREVER, MessageQueuePriority::Normal)
+            .unwrap();
     })
     .unwrap();
 
     task_spawn("rec", 90, move || {
         while let Some(rec) = channel.receive(WAIT_FOREVER).unwrap() {
             println!("rec: {}", rec);
+            ack_channel
+                .send(true, WAIT_FOREVER, MessageQueuePriority::Normal)
+                .unwrap();
         }
     })
     .unwrap();
